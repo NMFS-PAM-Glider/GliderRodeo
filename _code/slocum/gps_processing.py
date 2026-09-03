78ad8fcfb179984edb22367a2f8b770cfb20be3c
Kourtney Burger <kourtney.burger@noaa.gov>
2026-09-03 17:27:44 +0000

    updated capex gps

--- _code/slocum/gps_processing.py
+++ _code/slocum/gps_processing.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # read in profiles csv from cloud
-glider = 'risso'
-coords = pd.read_csv(f'gcs-mnt/swfscesd-glider-deployments-data-out/2026/{glider}-20260128/processed-L0/{glider}-20260128-delayed-profiles.csv')
+glider = 'capex987'
+coords = pd.read_csv(f'gcs-mnt/nmfs-collaborative-working/2026_GliderRodeo/Data/{glider}_20260128/esd data structure/data-out/2026/{glider}-20260128/processed-L0/{glider}-20260128-delayed-profiles.csv')
 
 # subset variables of interest and rename
 surfacing_coords = coords.loc[
@@ -20,32 +20,32 @@ surfacing_coords['endTime'] = surfacing_coords['endTime'].str.strip()
 surfacing_coords['endTime'] = pd.to_datetime(surfacing_coords['endTime'])
 
 # optional, cut off extra data points before and after rodeo
-cutoff = pd.to_datetime('2026-02-10 05:37')
-surfacing_coords = surfacing_coords[surfacing_coords['startTime'] <= cutoff]
+# cutoff = pd.to_datetime('2026-02-10 05:37')
+# surfacing_coords = surfacing_coords[surfacing_coords['startTime'] <= cutoff]
 
 # save to folder 
-surfacing_coords.to_csv(f'GliderRodeo/data/{glider}-20260128/{glider}-20260128_GPS_timeseries.csv', index=False)
+surfacing_coords.to_csv(f'GliderRodeo/data/{glider}_20260128/{glider}-20260128_GPS_timeseries.csv', index=False)
 
 
 # %% FOR TESTING
-# surfacing_coords = surfacing_coords.sort_values(by='start_time')
+surfacing_coords = surfacing_coords.sort_values(by='startTime')
 
-# plt.figure(figsize=(8, 6))
-# plt.plot(
-#     surfacing_coords['Longitude'], 
-#     surfacing_coords['Latitude'], 
-#     marker='o',       # Adds a dot for every surfacing event
-#     linestyle='-',    # Connects the dots with a line to show the path
-#     color='b',        # Blue color
-#     alpha=0.7         # Slight transparency
-# )
+plt.figure(figsize=(8, 6))
+plt.plot(
+    surfacing_coords['longitude'], 
+    surfacing_coords['latitude'], 
+    marker='o',       # Adds a dot for every surfacing event
+    linestyle='-',    # Connects the dots with a line to show the path
+    color='b',        # Blue color
+    alpha=0.7         # Slight transparency
+)
 
-# plt.title('Glider Surfacing Trajectory (stenella-20260128)')
-# plt.xlabel('Longitude')
-# plt.ylabel('Latitude')
-# plt.grid(True, linestyle='--', alpha=0.5)
+plt.title('Glider Surfacing Trajectory (stenella-20260128)')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.grid(True, linestyle='--', alpha=0.5)
 
-# plt.gca().set_aspect('equal', adjustable='datalim') 
+plt.gca().set_aspect('equal', adjustable='datalim') 
 
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
--- _code/slocum/sci_timeseries_processing.py
+++ _code/slocum/sci_timeseries_processing.py
@@ -3,9 +3,9 @@ import pandas as pd
 import xarray as xr
 
 # % load ESD Glider processed science timeseries, convert to dataframe, and reset index to inlcude time as reg column
-glider = 'stenella'
+glider = 'capex987'
 
-sci_time_ds = xr.open_dataset(f'gcs-mnt/swfscesd-glider-deployments-data-out/2026/{glider}-20260128/processed-L1/{glider}-20260128-delayed-sci.nc')
+sci_time_ds = xr.open_dataset(f'gcs-mnt/nmfs-collaborative-working/2026_GliderRodeo/Data/{glider}_20260128/esd data structure/data-out/2026/{glider}-20260128/processed-L1/{glider}-20260128-delayed-sci.nc')
 
 sci_time_df = sci_time_ds.to_dataframe()
 sci_time_df = sci_time_df.reset_index()
@@ -24,8 +24,8 @@ epoch_seconds = sci_time['time_utc'].astype('int64') // 10**9
 sci_time.insert(0, 'time', epoch_seconds)
 
 # optional, cut off extra data points before and after rodeo
-cutoff = pd.to_datetime('2026-02-10 05:37')
-sci_time = sci_time[sci_time['time_utc'] <= cutoff]
+# cutoff = pd.to_datetime('2026-02-10 05:37')
+# sci_time = sci_time[sci_time['time_utc'] <= cutoff]
 
 # save as csv
-sci_time.to_csv(f'GliderRodeo/data/{glider}-20260128/{glider}-20260128_science_timeseries.csv', index=False)
+sci_time.to_csv(f'GliderRodeo/data/{glider}_20260128/{glider}-20260128_science_timeseries.csv', index=False)
