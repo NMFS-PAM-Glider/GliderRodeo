5147d33dcce0381115e63674f7b99b2f9351c38f
Kourtney Burger <kourtney.burger@noaa.gov>
2026-09-08 00:59:35 +0000

    added stenella sci timeseries

--- _code/slocum/eng_timeseries_processing.py
+++ _code/slocum/eng_timeseries_processing.py
@@ -2,29 +2,18 @@
 import pandas as pd
 import xarray as xr
 
-# % load ESD Glider processed engineering timeseries, convert to dataframe, and reset index to inlcude time as reg column
-glider = 'stenella'
+# set global vars
+GLIDER = 'risso'
+START_TIME_CUTOFF = '2026-01-28 23:15:00'  # Format: 'YYYY-MM-DD HH:MM:SS'
+END_TIME_CUTOFF = '2026-02-10 09:45:00'    # Format: 'YYYY-MM-DD HH:MM:SS'
 
-eng_time_ds = xr.open_dataset(f'gcs-mnt/swfscesd-glider-deployments-data-out/2026/{glider}-20260128/processed-L1/{glider}-20260128-delayed-eng.nc')
+eng_time_ds = xr.open_dataset(f'gcs-mnt/swfscesd-glider-deployments-data-out/2026/{GLIDER}-20260128/processed-L0/{GLIDER}-20260128-delayed-raw.nc')
 
 eng_time_df = eng_time_ds.to_dataframe()
 eng_time_df = eng_time_df.reset_index()
 
 # % extract vars of interest
-vars = [
-    'time', 'heading', 'pitch', 'roll', 'distance_over_ground', 'battery_voltage', 'battpos', 'amphr', 'total_amphr', 'measured_oil_volume'
-]
-
-# additional vars to add, work with Sam! 
-# speed, speed through water, ballast state - c_autoballast_state, commanded climb and dive oil vol, picth commands (c and d use_pitch), digifin position, 
-
-# additional seaglider vars, do we have these???
-# Horizontal speed
-# Vertical speed
-# Speed (speed through water)
-# East_displacement
-# North_displacement
-# Glide angle
+vars = ['time', 'm_depth', 'm_heading', 'm_pitch', 'm_roll', 'm_battery', 'm_battpos', 'm_coulomb_amphr', 'm_coulomb_amphr_total', 'm_coulomb_current', 'm_de_oil_vol', 'm_gps_lat', 'm_gps_lon', 'm_depth_rate', 'm_fin', 'm_speed', 'm_speed_avg', 'c_de_oil_vol', 'c_pitch', 'c_fin', 'profile_index', 'profile_direction']
 
 eng_time = eng_time_df[vars]
 eng_time = eng_time.rename(columns={'time': 'time_utc'})
@@ -33,9 +22,17 @@ eng_time = eng_time.rename(columns={'time': 'time_utc'})
 epoch_seconds = eng_time['time_utc'].astype('int64') // 10**9
 eng_time.insert(0, 'time', epoch_seconds)
 
-# optional, cut off extra data points before and after rodeo
-cutoff = pd.to_datetime('2026-02-10 05:37')
-eng_time = eng_time[eng_time['time_utc'] <= cutoff]
+# only keep data between cutoff_start and cutoff_end
+cutoff_start = pd.to_datetime(START_TIME_CUTOFF)
+cutoff_end = pd.to_datetime(END_TIME_CUTOFF)
+
+eng_time = eng_time[
+    (eng_time['time_utc'] >= cutoff_start) & 
+    (eng_time['time_utc'] <= cutoff_end)
+]
+
+# keep row with m_depth reading
+eng_time = eng_time.dropna(subset=['m_depth'])
 
 # save as csv
-eng_time.to_csv(f'GliderRodeo/data/{glider}-20260128/{glider}-20260128_flight_timeseries_engineering.csv', index=False)
+eng_time.to_csv(f'GliderRodeo/data/{GLIDER}_20260128/{GLIDER}-20260128_flight_timeseries_engineering.csv', index=False)
